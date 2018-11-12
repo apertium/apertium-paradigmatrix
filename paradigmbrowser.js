2d92473d8df95aee5d27a7106912c1903861410f
Avyay Varadarajan <avyayv@gmail.com>
2018-11-11 19:05:32 -0800

    added comments fixed some issues

--- paradigmbrowser.js
+++ paradigmbrowser.js
@@ -1,8 +1,11 @@
-var hiddenTags = []
 
-// This is where you should specify anything not to add
+var hiddenTags = []
 var tagsNotToAdd = []
 
+//Specify APY URL Here
+var APY_URL = 'https://beta.apertium.org/apy/'
+
+//Allows for Paradigm after you have typed the space bar
 $(document).ready(function () {
   $(window).keypress(function (e) {
     if (e.key === ' ' || e.key === 'Spacebar') {
@@ -12,116 +15,147 @@ $(document).ready(function () {
   })
 })
 
+//Function to get the tags given a jquery object
 function getTags(obj) {
   if($(obj).data('tags') != undefined) {
-    var tags = $(obj).data('tags').split('.');
+
+    $(obj).text("");
     var completedString = "";
-    for (let i = 0; i < tags.length; i++) {
-      completedString += "<" + tags[i] + ">";
+
+    $.each($(obj).data('tags').split('.'), function(index, value) {
+      completedString += "<" + value + ">";
+    });
+
+    if(hiddenTags.indexOf(completedString) > -1 == false){
+      hiddenTags.push(completedString);
     }
-    hiddenTags.push(completedString);
+
   }
 }
 
+//Function to edit the values of the tags
 function editTags(obj, find, change) {
+
+  //see if tags exist
   if($(obj).data('tags') != undefined) {
     var tags = $(obj).data('tags').split('.');
-    str = "<"
-    for(let x = 0; x < tags.length; x++) {
-      if(x < tags.length - 1) {
-        str += tags[x] + "><"
-      }else{
-        str += tags[x] + ">"
-      }
-    }
 
+
+    str = "<"
+
+    $.each($(tags), function(index, value) {
+      if(index < tags.length - 1) {
+        str += value + "><"
+      }else{
+        str += value + ">"
+      }
+    });
+
+    //See if tags matches the tag in the html
     if(str == find){
-      if ((change.indexOf('#') > -1) == false) {
-        $(obj).text(change);
+
+      //Find if the value should be added or not
+      if ((change.indexOf('#') > -1) == false && ($(obj).text().indexOf(change) > -1 == false)) {
+        $(obj).text($(obj).text() + change + " ");
       }
     }
   }
 }
 
+//Function to determine the tags to remove
 function removeTheseTags(obj) {
   if($(obj).data('remove-tags') != undefined) {
     var tags = $(obj).data('remove-tags').split('.');
     var completedString = "";
-    for (let i = 0; i < tags.length; i++) {
-      tagsNotToAdd.push(tags[i]);
-    }
+    $.each($(tags), function(index, value) {
+      tagsNotToAdd.push(value);
+    });
   }
 }
 
-function runThruGettingTags(arr) {
-  $('#table tr').each(function(){
-    $(this).find('td').each(function(){
-      getTags(this);
-    })
-  })
-  for(let i = 0; i < arr.length; i++) {
-    $(arr[i]).each(function(){
-      getTags(this);
-    })
-  }
-}
-
-function runThruEditingNames(arr, hiddenTagsValue, updatedValue) {
-  $('#table tr').each(function(){
-    $(this).find('td').each(function(){
-      editTags(this, hiddenTagsValue, updatedValue)
-    })
-  })
-  for(let i = 0; i < arr.length; i++) {
-    $(arr[i]).each(function(){
-      editTags(this, hiddenTagsValue, updatedValue)
-    })
-  }
-}
-
-function paradigm() {
-  $(document).ready(function () {
-    var language = $('#Language').val();
-    var paradigmText = $('#ParadigmText').val();
-    var languagesWithFirstTag = ""
-    $('p').each(function(){
-      removeTheseTags(this);
-    })
-    $.getJSON(encodeURI('https://beta.apertium.org/apy/analyze?lang='+language+'&q='+paradigmText),function(data,status) {
-      var arrOfWordsWithFirstTag = []
-      languagesWithFirstTag = data[0][0];
-      individualTypesOfWord = languagesWithFirstTag.split('/');
-      for(let i = 1; i < individualTypesOfWord.length; i++) {
-        text = individualTypesOfWord[i].replace(new RegExp('><', 'g'), ".");
-        text = text.replace(new RegExp('<', 'g'), ".");
-        text = text.replace(new RegExp('>', 'g'), ".");
-        text = text.split(/[\s.]+/)
-        string = ""
-        for(let j = 1; j < text.length; j++){
-          if(text[j] != ""){
-            if(tagsNotToAdd.indexOf(text[j].trim()) > -1 == false){
-              if(string.includes("<" + text[j] + ">") == false){
-                string += "<" + text[j] + ">";
-              }
-            }
-          }
-        }
-        arrOfWordsWithFirstTag.push(individualTypesOfWord[i].split('<')[0]+string);
-      }
-      languagesWithFirstTag = arrOfWordsWithFirstTag;
-      hiddenTags = []
-      runThruGettingTags(["body", "span", "div", "p"])
-      for (let j = 0; j < languagesWithFirstTag.length; j++) {
-        for(let k = 0; k < hiddenTags.length; k++) {
-          $.getJSON(encodeURI('https://beta.apertium.org/apy/generate?lang='+language+'&q='+languagesWithFirstTag[j]+hiddenTags[k]),function(data,status) {
-            runThruEditingNames(["p", "div", "body", "span"], hiddenTags[k], data[0][0])
-          },'html');
-        }
-      }
-    },'html');
+// Function to go through all tags and use the getTags function
+function runThruGettingTags() {
+  $("*").each(function(){
+    getTags(this);
   });
 }
 
-function onlyUnique(value, index, self) {
-    return self.indexOf(value) === index;
+// Function to see all tags and edit
+function runThruEditingNames(hiddenTagsValue, updatedValue) {
+
+  $("*").each(function(){
+    editTags(this, hiddenTagsValue, updatedValue)
+  });
+
+}
+
+//Function is called when button is pressed or when spacebar is pressed
+function paradigm() {
+
+  $(document).ready(function () {
+
+    //get values of the language and text to be paradigmed
+    var language = $('#Language').val();
+    var paradigmText = $('#ParadigmText').val();
+    var arrOfWordsWithWantedTags = ""
+
+    //go through all elements and see to remove all not
+    $('*').each(function(){
+      removeTheseTags(this);
+    })
+
+    //get JSON from the analyze endpoint to see the different forms of the word
+    $.getJSON(encodeURI(APY_URL + 'analyze?lang='+language+'&q='+paradigmText), function(data,status) {
+
+      var arrOfWordsWithWantedTags = []
+      arrOfWordsWithWantedTags = data[0][0];
+      forms = arrOfWordsWithWantedTags.split('/');
+
+      //iterate through all the forms of the word
+      $.each($(forms), function(index, value) {
+        if(index > 0) {
+
+          //get the forms in array form
+          text = value.replace(new RegExp('><', 'g'), ".");
+          text = text.replace(new RegExp('<', 'g'), ".");
+          text = text.replace(new RegExp('>', 'g'), ".");
+          text = text.split(/[\s.]+/)
+
+          string = ""
+
+          //get all the tags now with the tags we wanted removed, removed
+          $.each($(text), function(insideIndex, indString) {
+            if(insideIndex > 0 && indString != "" && tagsNotToAdd.indexOf(indString.trim()) > -1 == false && string.includes("<" + indString + ">") == false){
+              string += "<" + indString + ">";
+            }
+          });
+
+          //get the form of the word so it is ready to be queryed to generate endpoint
+          arrOfWordsWithWantedTags.push(value.split('<')[0]+string);
+
+          //make sure that the hiddenTags variable does not add elements twice; clear the array
+          hiddenTags = []
+
+          //get all tags and store in hiddenTags Variable
+          runThruGettingTags()
+
+          //iterate through words with base tags and add the hidden tags on the html onto this
+          $.each($(arrOfWordsWithWantedTags), function(firstTagIndex, firstTag) {
+            $.each($(hiddenTags), function(hiddenTagIndex, hiddenTag) {
+
+              //query generate endpoint so we can see the end values
+              $.getJSON(encodeURI(APY_URL + 'generate?lang='+language+'&q='+firstTag+hiddenTag),function(data,status) {
+
+                //edit html values from the output of the APY
+                runThruEditingNames(hiddenTag, data[0][0])
+              },'html');
+
+            });
+          });
+
+        }
+      });
+    },'html');
+  });
+
 }
