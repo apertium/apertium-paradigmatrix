8652d678067b0adba69de9202dbda7be56857184
Hossep Dolatian <hovdeov@gmail.com>
2022-03-14 15:24:47 -0700

    added hye

--- langs/hye.js
+++ langs/hye.js
@@ -1,4 +1,4 @@
-function hyw_fin_vb(tense_tgs, mood_tgs, lab) {
+function hye_fin_vb(tense_tgs, mood_tgs, lab) {
   return {
     id: tense_tgs.replaceAll('.', '-'),
     id: mood_tgs.replaceAll('.', '-'),
@@ -19,10 +19,8 @@ function hyw_fin_vb(tense_tgs, mood_tgs, lab) {
     ]
   };
 }
-function hyw_fin_vb_fut(tense_tgs, mood_tgs, lab) {
+function hye_fin_vb_conditional_pres(lab) {
   return {
-    id: tense_tgs.replaceAll('.', '-'),
-    id: mood_tgs.replaceAll('.', '-'),
     label: lab,
     tabcolgroups: [
       {label: 'Affirmative', width: 2},
@@ -31,61 +29,36 @@ function hyw_fin_vb_fut(tense_tgs, mood_tgs, lab) {
     tabcols: ['Singular', 'Plural', 'Singular', 'Plural'],
     tabrows: ['First person', 'Second person',  'Third person'],
     tabdata: [
-      [{pretxt: 'պիտի', tags: tense_tgs+'.p1.sg.'+mood_tgs},        {pretxt: 'պիտի', tags: tense_tgs+'.p1.pl.'+mood_tgs},
-       {pretxt: 'պիտի', tags: tense_tgs+'.p1.sg.'+mood_tgs+'.neg'}, {pretxt: 'պիտի', tags: tense_tgs+'.p1.pl.'+mood_tgs+'.neg'}],
-      [{pretxt: 'պիտի', tags: tense_tgs+'.p2.sg.'+mood_tgs},        {pretxt: 'պիտի', tags: tense_tgs+'.p2.pl.'+mood_tgs},
-       {pretxt: 'պիտի', tags: tense_tgs+'.p2.sg.'+mood_tgs+'.neg'}, {pretxt: 'պիտի', tags: tense_tgs+'.p2.pl.'+mood_tgs+'.neg'}],
-      [{pretxt: 'պիտի', tags: tense_tgs+'.p3.sg.'+mood_tgs},        {pretxt: 'պիտի', tags: tense_tgs+'.p3.pl.'+mood_tgs},
-       {pretxt: 'պիտի', tags: tense_tgs+'.p3.sg.'+mood_tgs+'.neg'}, {pretxt: 'պիտի', tags: tense_tgs+'.p3.pl.'+mood_tgs+'.neg'}]
+      [{tags: 'pres.p1.sg.subj.cond'}, {tags: 'pres.p1.pl.subj.cond'},
+       {pretxt: 'չեմ', tags: 'neg_ptcp'}, {pretxt: 'չենք', tags: 'neg_ptcp'}],
+      [{tags: 'pres.p2.sg.subj.cond'}, {tags: 'pres.p2.pl.subj.cond'},
+       {pretxt: 'չես', tags: 'neg_ptcp'}, {pretxt: 'չեք', tags: 'neg_ptcp'}],
+      [{tags: 'pres.p3.sg.subj.cond'}, {tags: 'pres.p3.pl.subj.cond'},
+       {pretxt: 'չի', tags: 'neg_ptcp'}, {pretxt: 'չեն', tags: 'neg_ptcp'}]
+    ]
+  };
+}
+function hye_fin_vb_conditional_past(lab) {
+  return {
+    label: lab,
+    tabcolgroups: [
+      {label: 'Affirmative', width: 2},
+      {label: 'Negative', width: 2}
+    ],
+    tabcols: ['Singular', 'Plural', 'Singular', 'Plural'],
+    tabrows: ['First person', 'Second person',  'Third person'],
+    tabdata: [
+      [{tags: 'past.impf.p1.sg.subj.cond'}, {tags: 'past.impf.p1.pl.subj.cond'},
+       {pretxt: 'չէի', tags: 'neg_ptcp'}, {pretxt: 'չէինք', tags: 'neg_ptcp'}],
+      [{tags: 'past.impf.p2.sg.subj.cond'}, {tags: 'past.impf.p2.pl.subj.cond'},
+       {pretxt: 'չէիր', tags: 'neg_ptcp'}, {pretxt: 'չէիք', tags: 'neg_ptcp'}],
+      [{tags: 'past.impf.p3.sg.subj.cond'}, {tags: 'past.impf.p3.pl.subj.cond'},
+       {pretxt: 'չէր', tags: 'neg_ptcp'}, {pretxt: 'չէին', tags: 'neg_ptcp'}]
     ]
   };
 }
 
-function hyw_fin_vb_periphrasis_pres_chi(tense_tgs, mood_tgs, verb_part_form, lab) {
-  return {
-    id: tense_tgs.replaceAll('.', '-'),
-    id: mood_tgs.replaceAll('.', '-'),
-    id: verb_part_form.replaceAll('.', '-'),
-    label: lab,
-    tabcolgroups: [
-      {label: 'Affirmative', width: 2},
-      {label: 'Negative', width: 2}
-    ],
-    tabcols: ['Singular', 'Plural', 'Singular', 'Plural'],
-    tabrows: ['First person', 'Second person',  'Third person'],
-    tabdata: [
-      [{tags: tense_tgs+'.p1.sg.'+mood_tgs}, {tags: tense_tgs+'.p1.pl.'+mood_tgs},
-       {pretxt: 'չեմ', tags: tense_tgs+'.'+verb_part_form}, {pretxt: 'չենք', tags: tense_tgs+'.'+verb_part_form}],
-      [{tags: tense_tgs+'.p2.sg.'+mood_tgs}, {tags: tense_tgs+'.p2.pl.'+mood_tgs},
-       {pretxt: 'չես', tags: tense_tgs+'.'+verb_part_form}, {pretxt: 'չէք', tags: tense_tgs+'.'+verb_part_form}],
-      [{tags: tense_tgs+'.p3.sg.'+mood_tgs}, {tags: tense_tgs+'.p3.pl.'+mood_tgs},
-       {pretxt: 'չի', tags: tense_tgs+'.'+verb_part_form}, {pretxt: 'չեն', tags: tense_tgs+'.'+verb_part_form}]
-      ]
-  };
-}
-function hyw_fin_vb_periphrasis_past(tense_tgs, mood_tgs, verb_part_form, lab) {
-  return {
-    id: tense_tgs.replaceAll('.', '-'),
-    id: mood_tgs.replaceAll('.', '-'),
-    id: verb_part_form.replaceAll('.', '-'),
-    label: lab,
-    tabcolgroups: [
-      {label: 'Affirmative', width: 2},
-      {label: 'Negative', width: 2}
-    ],
-    tabcols: ['Singular', 'Plural', 'Singular', 'Plural'],
-    tabrows: ['First person', 'Second person',  'Third person'],
-    tabdata: [
-      [{tags: tense_tgs+'.p1.sg.'+mood_tgs}, {tags: tense_tgs+'.p1.pl.'+mood_tgs},
-       {pretxt: 'չէի', tags: tense_tgs+'.'+verb_part_form}, {pretxt: 'չէինք', tags: tense_tgs+'.'+verb_part_form}],
-      [{tags: tense_tgs+'.p2.sg.'+mood_tgs}, {tags: tense_tgs+'.p2.pl.'+mood_tgs},
-       {pretxt: 'չէիր', tags: tense_tgs+'.'+verb_part_form}, {pretxt: 'չէիք', tags: tense_tgs+'.'+verb_part_form}],
-      [{tags: tense_tgs+'.p3.sg.'+mood_tgs}, {tags: tense_tgs+'.p3.pl.'+mood_tgs},
-       {pretxt: 'չէր', tags: tense_tgs+'.'+verb_part_form}, {pretxt: 'չէին', tags: tense_tgs+'.'+verb_part_form}]
-      ]
-  };
-}
-function hyw_fin_vb_perf_pres(verb_part_form, lab) {
+function hye_fin_vb_periphrasis_general_pres(verb_part_form, lab) {
   return {
     id: verb_part_form.replaceAll('.', '-'),
     label: lab,
@@ -101,11 +74,11 @@ function hyw_fin_vb_perf_pres(verb_part_form, lab) {
       [{tags: verb_part_form, posttxt: "ես"}, {tags: verb_part_form, posttxt: "էք"},
        {pretxt: 'չես', tags: verb_part_form}, {pretxt: 'չէք', tags: verb_part_form}],
       [{tags: verb_part_form, posttxt: "է"}, {tags: verb_part_form, posttxt: "են"},
-       {pretxt: 'չէ', tags: verb_part_form}, {pretxt: 'չեն', tags: verb_part_form}]
+       {pretxt: 'չի', tags: verb_part_form}, {pretxt: 'չեն', tags: verb_part_form}]
       ]
   };
 }
-function hyw_fin_vb_perf_past(verb_part_form, lab) {
+function hye_fin_vb_periphrasis_general_past(verb_part_form, lab) {
   return {
     id: verb_part_form.replaceAll('.', '-'),
     label: lab,
@@ -125,7 +98,7 @@ function hyw_fin_vb_perf_past(verb_part_form, lab) {
       ]
   };
 }
-function hyw_noun_decline_bare(det_tag, lab) {
+function hye_noun_decline_bare(det_tag, lab) {
   return {
     /*id: det_tag.replaceAll('.', '-'),*/
     label: lab,
@@ -139,7 +112,7 @@ function hyw_noun_decline_bare(det_tag, lab) {
     ]
   };
 }
-function hyw_noun_decline(det_tag, lab) {
+function hye_noun_decline(det_tag, lab) {
   return {
     id: det_tag.replaceAll('.', '-'),
     label: lab,
@@ -153,7 +126,7 @@ function hyw_noun_decline(det_tag, lab) {
     ]
   };
 }
-function hyw_noun_decline_pl_poss(det_tag, lab) {
+function hye_noun_decline_pl_poss(det_tag, lab) {
   return {
     id: det_tag.replaceAll('.', '-'),
     label: lab,
@@ -167,7 +140,7 @@ function hyw_noun_decline_pl_poss(det_tag, lab) {
     ]
   };
 }
-function hyw_adj_decline_bare(det_tag, lab) {
+function hye_adj_decline_bare(det_tag, lab) {
   return {
     /*id: det_tag.replaceAll('.', '-'),*/
     label: lab,
@@ -181,7 +154,7 @@ function hyw_adj_decline_bare(det_tag, lab) {
     ]
   };
 }
-function hyw_adj_decline(det_tag, lab) {
+function hye_adj_decline(det_tag, lab) {
   return {
     id: det_tag.replaceAll('.', '-'),
     label: lab,
@@ -195,7 +168,7 @@ function hyw_adj_decline(det_tag, lab) {
     ]
   };
 }
-function hyw_adj_decline_pl_poss(det_tag, lab) {
+function hye_adj_decline_pl_poss(det_tag, lab) {
   return {
     id: det_tag.replaceAll('.', '-'),
     label: lab,
@@ -210,7 +183,7 @@ function hyw_adj_decline_pl_poss(det_tag, lab) {
   };
 }
 
-function add_hyx_hyw() {
+function add_hyx_hye() {
   return {
     verb: [
       {
@@ -218,32 +191,35 @@ function add_hyx_hyw() {
         label: 'Non-finite forms',
         tabcols: ['Affirmative', 'Negative'],
         tabrows: ["Infinitive", "Subject participle", "Resultative participle",
-         "Evidential participle","Future converb 1","Future converb 2",
-         "Connegative converb (present)", "Connegative converb (past imperfect)"],
+         "Imperfective converb","Simultaneous converb","Perfective converb",
+          "Future converb 1","Future converb 2",
+         "Connegative converb"],
         tabdata: [
           [{tags: 'ger'}, {tags: 'ger.neg'}],
           [{tags: 'pprs'}, {tags: 'pprs.neg'}],
           [{tags: 'res'}, {tags: 'res.neg'}],
-          [{tags: 'evid', width: 2}],
+          [{tags: 'impf_cvb', width: 2}],
+          [{tags: 'ger.sim_ptcp', width: 2}],
+          [{tags: 'perf', width: 2}],
           [{tags: 'ger.fut_ptcp', width: 2}],
           [{tags: 'ger.pros_ptcp', width: 2}],
-          [{tags: 'pres.neg_ptcp', width: 2}],
-          [{tags: 'past.impf.neg_ptcp', width: 2}]
+          [{tags: 'neg_ptcp', width: 2}],
         ]
       },
-      hyw_fin_vb('pres', 'indc', 'Indicative present (if defective)'),
-      hyw_fin_vb_periphrasis_pres_chi('pres', 'indc','neg_ptcp', 'Indicative present (regular)'),
-      hyw_fin_vb('past.impf', 'indc', 'Indicative past imperfect (if defective)'),
-      hyw_fin_vb_periphrasis_past('past.impf', 'indc','neg_ptcp', 'Indicative past imperfect (regular)'),
-      hyw_fin_vb('pres', 'subj', 'Subjunctive present'),
-      hyw_fin_vb('past.impf', 'subj', 'Subjunctive past imperfect'),
-      hyw_fin_vb_fut('pres', 'subj', 'Future'),
-      hyw_fin_vb_fut('past.impf', 'subj', 'Future perfect'),
-      hyw_fin_vb_perf_pres('res', 'Present perfect (non-evidential)'),
-      hyw_fin_vb_perf_past('res', 'Pluperfect (non-evidential)'),
-      hyw_fin_vb_perf_pres('evid', 'Present perfect (evidential)'),
-      hyw_fin_vb_perf_past('evid', 'Pluperfect (evidential)'),
-      hyw_fin_vb('past.pret', 'indc', 'Aorist (simple past)'),
+      hye_fin_vb('pres', 'indc', 'Indicative present (if defective)'),
+      hye_fin_vb_periphrasis_general_pres('impf_cvb', 'Indicative present (regular)'),
+      hye_fin_vb('past.impf', 'indc', 'Indicative past imperfect (if defective)'),
+      hye_fin_vb_periphrasis_general_past('impf_cvb', 'Indicative past imperfect (regular)'),
+      hye_fin_vb_periphrasis_general_pres('ger.fut_ptcp', 'Future'),
+      hye_fin_vb_periphrasis_general_past('ger.fut_ptcp', 'Future perfect'),
+      hye_fin_vb_periphrasis_general_pres('perf', 'Present perfect'),
+      hye_fin_vb_periphrasis_general_past('perf', 'Past perfect'),
+      hye_fin_vb('past.pret', 'indc', 'Aorist (simple past)'),
+      hye_fin_vb('pres', 'subj', 'Subjunctive present'),
+      hye_fin_vb('past.impf', 'subj', 'Subjunctive past imperfect'),
+      hye_fin_vb_conditional_pres('Conditional future'),
+      hye_fin_vb_conditional_past('Conditional future perfect'),
+  
       {
         id: 'imp',
         label: 'Imperative',
@@ -255,45 +231,45 @@ function add_hyx_hyw() {
         tabrows: ['First person', 'Second person'],
         tabdata: [
           [{tags: 'imp.p2.sg'}, {tags: 'imp.p2.pl'},
-         {pretxt: 'մի', tags: 'proh.p2.sg'},  {pretxt: 'մի', tags: 'proh.p2.pl'}]
+         {pretxt: 'մի', tags: 'imp.p2.sg'},  {pretxt: 'մի', tags: 'imp.p2.pl'}]
         ]
       },
     ],
     noun: [
-      hyw_noun_decline_bare('', 'Simple noun'),
-      hyw_noun_decline('def','Definite noun'),
-      hyw_noun_decline('px1sg','1st possessed noun'),
-      hyw_noun_decline('px2sg','2nd possessive noun'),
-      hyw_noun_decline_pl_poss('px1sg','Noun with 1st person plural possessor'),
-      hyw_noun_decline_pl_poss('px2sg', 'Noun with 2nd person plural possessor'),
-      hyw_noun_decline_pl_poss('def','Noun with 3rd person plural possessor'),
+      hye_noun_decline_bare('', 'Simple noun'),
+      hye_noun_decline('def','Definite noun'),
+      hye_noun_decline('px1sg','1st possessed noun'),
+      hye_noun_decline('px2sg','2nd possessive noun'),
+      hye_noun_decline_pl_poss('px1sg','Noun with 1st person plural possessor'),
+      hye_noun_decline_pl_poss('px2sg', 'Noun with 2nd person plural possessor'),
+      hye_noun_decline_pl_poss('def','Noun with 3rd person plural possessor'),
     ],
     nounproper: [
-      hyw_noun_decline_bare('', 'Nominalized proper noun'),
-      hyw_noun_decline('def','Definite proper noun'),
-      hyw_noun_decline('px1sg','1st possessed proper noun'),
-      hyw_noun_decline('px2sg','2nd possessive proper noun'),
-      hyw_noun_decline_pl_poss('px1sg','Proper noun with 1st person plural possessor'),
-      hyw_noun_decline_pl_poss('px2sg', 'Proper noun with 2nd person plural possessor'),
-      hyw_noun_decline_pl_poss('def','Proper noun with 3rd person plural possessor'),
+      hye_noun_decline_bare('', 'Nominalized proper noun'),
+      hye_noun_decline('def','Definite proper noun'),
+      hye_noun_decline('px1sg','1st possessed proper noun'),
+      hye_noun_decline('px2sg','2nd possessive proper noun'),
+      hye_noun_decline_pl_poss('px1sg','Proper noun with 1st person plural possessor'),
+      hye_noun_decline_pl_poss('px2sg', 'Proper noun with 2nd person plural possessor'),
+      hye_noun_decline_pl_poss('def','Proper noun with 3rd person plural possessor'),
     ],
     adj: [
-      hyw_adj_decline_bare('', 'Nominalized adjective'),
-      hyw_adj_decline('def','Definite adjective'),
-      hyw_adj_decline('px1sg','1st possessed adjective'),
-      hyw_adj_decline('px2sg','2nd possessive adjective'),
-      hyw_adj_decline_pl_poss('px1sg','Adjective with 1st person plural possessor'),
-      hyw_adj_decline_pl_poss('px2sg', 'Adjective with 2nd person plural possessor'),
-      hyw_adj_decline_pl_poss('def','Adjective with 3rd person plural possessor'),
+      hye_adj_decline_bare('', 'Nominalized adjective'),
+      hye_adj_decline('def','Definite adjective'),
+      hye_adj_decline('px1sg','1st possessed adjective'),
+      hye_adj_decline('px2sg','2nd possessive adjective'),
+      hye_adj_decline_pl_poss('px1sg','Adjective with 1st person plural possessor'),
+      hye_adj_decline_pl_poss('px2sg', 'Adjective with 2nd person plural possessor'),
+      hye_adj_decline_pl_poss('def','Adjective with 3rd person plural possessor'),
     ],
     adv: [
-      hyw_adj_decline_bare('', 'Nominalized adverb'),
-      hyw_adj_decline('def','Definite adverb'),
-      hyw_adj_decline('px1sg','1st possessed adverb'),
-      hyw_adj_decline('px2sg','2nd possessive adverb'),
-      hyw_adj_decline_pl_poss('px1sg','Adverb with 1st person plural possessor'),
-      hyw_adj_decline_pl_poss('px2sg', 'Adverb with 2nd person plural possessor'),
-      hyw_adj_decline_pl_poss('def','Adverb with 3rd person plural possessor'),
+      hye_adj_decline_bare('', 'Nominalized adverb'),
+      hye_adj_decline('def','Definite adverb'),
+      hye_adj_decline('px1sg','1st possessed adverb'),
+      hye_adj_decline('px2sg','2nd possessive adverb'),
+      hye_adj_decline_pl_poss('px1sg','Adverb with 1st person plural possessor'),
+      hye_adj_decline_pl_poss('px2sg', 'Adverb with 2nd person plural possessor'),
+      hye_adj_decline_pl_poss('def','Adverb with 3rd person plural possessor'),
     ]
   };
 }
--- paradigmbrowser.js
+++ paradigmbrowser.js
@@ -28,6 +28,7 @@ var LANGS = {
     grc: {name: "Ancient Greek", data: null, keeptags: []},
     kaz: {name: "Kazakh", data: null, keeptags: ['tv', 'iv']},
     hyx_hyw: {name: "Western Armenian", data: null, keeptags: ['tv', 'iv','inch','pass_listed','pass','caus','caus_listed','subst','ant','m','f','al','cog','top']},
+    hyx_hye: {name: "Eastern Armenian", data: null, keeptags: ['tv', 'iv','inch','pass_listed','pass','caus','caus_listed','subst','ant','m','f','al','cog','top']},
     spa: {name: "Spanish", data: null, keeptags: []}
 };
 
