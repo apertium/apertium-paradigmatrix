388f3f23d7b06e9ca36b0cac8f0deaca63d8b0e5
Jonathan Washington <jonathan.n.washington@gmail.com>
2025-04-03 19:22:06 -0400

    fixed to new verb system

--- langs/unm.js
+++ langs/unm.js
@@ -4,31 +4,31 @@ function add_unm() {
       {
         id: 'ind',
         label: 'Indicative',
-        tabcols: ['Singular Object', 'Plural Object'],
+        tabcols: ['Singular Object', 'Plural Object', 'Indefinite Object'],
         tabrows: ['First person sg subj', 'Second person sg subj', 'Third person sg subj', 'First person pl (excl) subj', 'First person pl (incl) subj', 'Second person pl subj', 'Third person pl subj' ],
         tabdata: [
-          [{tags: 'p1.sg.o_sg'}, {tags: 'p1.sg.o_pl'}],
-          [{tags: 'p2.sg.o_sg'}, {tags: 'p2.sg.o_pl'}],
-          [{tags: 'p3.sg.o_sg'}, {tags: 'p3.sg.o_pl'}],
-          [{tags: 'p1.excl.pl.o_sg'}, {tags: 'p1.excl.pl.o_pl'}],
-          [{tags: 'p1.incl.pl.o_sg'}, {tags: 'p1.incl.pl.o_pl'}],
-          [{tags: 'p2.pl', width: 2}],
-          [{tags: 'p3.pl', width: 2}]
+          [{tags: 's_1sg.o_sg'}, {tags: 's_1sg.o_pl'}, {tags: 's_1sg'}],
+          [{tags: 's_2sg.o_sg'}, {tags: 's_2sg.o_pl'}, {tags: 's_2sg'}],
+          [{tags: 's_3sg.o_sg'}, {tags: 's_3sg.o_pl'}, {tags: 's_3sg'}],
+          [{tags: 's_11pl.o_sg'}, {tags: 's_11pl.o_pl'}, {tags: 's_11pl'}],
+          [{tags: 's_12pl.o_sg'}, {tags: 's_12pl.o_pl'}, {tags: 's_12pl'}],
+          [{tags: 's_2pl', width: 2}, {tags: 's_2pl'}],
+          [{tags: 's_3pl', width: 2}, {tags: 's_3pl'}]
         ]
       },
       {
         id: 'subord',
         label: 'Subordinative',
-        tabcols: ['Singular Object', 'Plural Object'],
+        tabcols: ['Singular Object', 'Plural Object', 'Indefinite Object'],
         tabrows: ['First person sg subj', 'Second person sg subj', 'Third person sg subj', 'First person pl (excl) subj', 'First person pl (incl) subj', 'Second person pl subj', 'Third person pl subj' ],
         tabdata: [
-          [{tags: 'subord.p1.sg.o_sg'}, {tags: 'subord.p1.sg.o_pl'}],
-          [{tags: 'subord.p2.sg.o_sg'}, {tags: 'subord.p2.sg.o_pl'}],
-          [{tags: 'subord.p3.sg.o_sg'}, {tags: 'subord.p3.sg.o_pl'}],
-          [{tags: 'subord.p1.excl.pl.o_sg'}, {tags: 'subord.p1.excl.pl.o_pl'}],
-          [{tags: 'subord.p1.incl.pl.o_sg'}, {tags: 'subord.p1.incl.pl.o_pl'}],
-          [{tags: 'subord.p2.pl', width: 2}],
-          [{tags: 'subord.p3.pl', width: 2}]
+          [{tags: 'subord.s_1sg.o_sg'}, {tags: 'subord.s_1sg.o_pl'}, {tags: 'subord.s_1sg'}],
+          [{tags: 'subord.s_2sg.o_sg'}, {tags: 'subord.s_2sg.o_pl'}, {tags: 'subord.s_2sg'}],
+          [{tags: 'subord.s_3sg.o_sg'}, {tags: 'subord.s_3sg.o_pl'}, {tags: 'subord.s_3sg'}],
+          [{tags: 'subord.s_11pl.o_sg'}, {tags: 'subord.s_11pl.o_pl'}, {tags: 'subord.s_11pl'}],
+          [{tags: 'subord.s_12pl.o_sg'}, {tags: 'subord.s_12pl.o_pl'}, {tags: 'subord.s_12pl'}],
+          [{tags: 'subord.s_2pl', width: 2}, {tags: 'subord.s_2pl'}],
+          [{tags: 'subord.s_3pl', width: 2}, {tags: 'subord.s_3pl'}]
         ]
       },
     ],
@@ -39,10 +39,10 @@ function add_unm() {
         tabcols: ['Singular', 'Plural'],
         tabrows: ['First person', 'First person (incl)', 'Second person', 'Third person'],
         tabdata: [
-          [{tags: 'p1.sg'}, {tags: 'p1.excl.pl'}],
-          [{tags: ''}, {tags: 'p1.incl.pl'}],
-          [{tags: 'p2.sg'}, {tags: 'p2.pl'}],
-          [{tags: 'p3.sg'}, {tags: 'p3.pl'}]
+          [{tags: 's_1sg'}, {tags: 's_11pl'}],
+          [{tags: ''}, {tags: 's_12pl'}],
+          [{tags: 's_2sg'}, {tags: 's_2pl'}],
+          [{tags: 's_3sg'}, {tags: 's_3pl'}]
         ]
       },
       {
@@ -51,10 +51,10 @@ function add_unm() {
         tabcols: ['Singular', 'Plural'],
         tabrows: ['First person', 'First person (incl)', 'Second person', 'Third person'],
         tabdata: [
-          [{tags: 'subord.p1.sg'}, {tags: 'subord.p1.excl.pl'}],
-          [{tags: ''}, {tags: 'subord.p1.incl.pl'}],
-          [{tags: 'subord.p2.sg'}, {tags: 'subord.p2.pl'}],
-          [{tags: 'subord.p3.sg'}, {tags: 'subord.p3.pl'}]
+          [{tags: 'subord.s_1sg'}, {tags: 'subord.s_11pl'}],
+          [{tags: ''}, {tags: 'subord.s_12pl'}],
+          [{tags: 'subord.s_2sg'}, {tags: 'subord.s_2pl'}],
+          [{tags: 'subord.s_3sg'}, {tags: 'subord.s_3pl'}]
         ]
       },
     ],
@@ -65,13 +65,13 @@ function add_unm() {
         tabcols: ['... him/her/it', '... them'],
         tabrows: ['First person sg subj', 'Second person sg subj', 'Third person sg subj', 'First person pl (excl) subj', 'First person pl (incl) subj', 'Second person pl subj', 'Third person pl subj' ],
         tabdata: [
-          [{tags: 'dir.p1.sg.o_sg'}, {tags: 'dir.p1.sg.o_pl'}],
-          [{tags: 'dir.p2.sg.o_sg'}, {tags: 'dir.p2.sg.o_pl'}],
-          [{tags: 'dir.p3.sg.o_sg'}, {tags: 'dir.p3.sg.o_pl'}],
-          [{tags: 'dir.p1.excl.pl.o_sg'}, {tags: 'dir.p1.excl.pl.o_pl'}],
-          [{tags: 'dir.p1.incl.pl.o_sg'}, {tags: 'dir.p1.incl.pl.o_pl'}],
-          [{tags: 'dir.p2.pl.o_sg'}, {tags: 'dir.p2.pl.o_pl'}],
-          [{tags: 'dir.p3.pl', width: 2}]
+          [{tags: 'dir.s_1sg.o_sg'}, {tags: 'dir.s_1sg.o_pl'}],
+          [{tags: 'dir.s_2sg.o_sg'}, {tags: 'dir.s_2sg.o_pl'}],
+          [{tags: 'dir.s_3sg.o_sg'}, {tags: 'dir.s_3sg.o_pl'}],
+          [{tags: 'dir.s_11pl.o_sg'}, {tags: 'dir.s_11pl.o_pl'}],
+          [{tags: 'dir.s_12pl.o_sg'}, {tags: 'dir.s_12pl.o_pl'}],
+          [{tags: 'dir.s_2pl.o_sg'}, {tags: 'dir.s_2pl.o_pl'}],
+          [{tags: 'dir.s_3pl', width: 2}]
         ]
       },
       {
@@ -80,13 +80,13 @@ function add_unm() {
         tabcols: ['He/she ...', 'They ...'],
         tabrows: ['First person sg obj', 'Second person sg obj', 'Third person sg obj', 'First person pl (excl) obj', 'First person pl (incl) obj', 'Second person pl obj', 'Third person pl obj' ],
         tabdata: [
-          [{tags: 'inv.p1.sg.s_sg'}, {tags: 'inv.p1.sg.s_pl'}],
-          [{tags: 'inv.p2.sg.s_sg'}, {tags: 'inv.p2.sg.s_pl'}],
-          [{tags: 'inv.p3.sg.s_sg'}, {tags: 'inv.p3.sg.s_pl'}],
-          [{tags: 'inv.p1.excl.pl.s_sg'}, {tags: 'inv.p1.excl.pl.s_pl'}],
-          [{tags: 'inv.p1.incl.pl.s_sg'}, {tags: 'inv.p1.incl.pl.s_pl'}],
-          [{tags: 'inv.p2.pl.s_sg'}, {tags: 'inv.p2.pl.s_pl'}],
-          [{tags: 'inv.p3.pl', width: 2}]
+          [{tags: 'inv.o_1sg.s_sg'}, {tags: 'inv.o_1sg.s_pl'}],
+          [{tags: 'inv.o_2sg.s_sg'}, {tags: 'inv.o_2sg.s_pl'}],
+          [{tags: 'inv.o_3sg.s_sg'}, {tags: 'inv.o_3sg.s_pl'}],
+          [{tags: 'inv.o_11pl.s_sg'}, {tags: 'inv.o_11pl.s_pl'}],
+          [{tags: 'inv.o_12pl.s_sg'}, {tags: 'inv.o_12pl.s_pl'}],
+          [{tags: 'inv.o_2pl.s_sg'}, {tags: 'inv.o_2pl.s_pl'}],
+          [{tags: 'inv.o_3pl', width: 2}]
         ]
       },
     ]
